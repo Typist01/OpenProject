961622362b42f2932e44ac0a555d9af14785b9fc
Mustafa <99654337+Typist01@users.noreply.github.com>
2022-10-02 17:04:59 +0100

    project slice added

--- frontend/src/store/projectSlice.ts
+++ frontend/src/store/projectSlice.ts
@@ -1,22 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit"
-import task from "./Models/task"
-
+import { DonutLarge, SystemSecurityUpdate } from "@mui/icons-material";
+import { createSlice } from "@reduxjs/toolkit";
+import { time } from "console";
+import task from "./Models/task";
 
 interface projectState {
-    id: string;
-    tasks: task[];
+  id: string;
+  tasks: task[];
 }
-const projectSlice = createSlice({
-    name: "project",
-    initialState: {
-        id: "0",
-        tasks: []
-    }
-    reducers: {
-        addTask(state, payload) {
-            newTask = {payload}
-                state.tasks = [...state.tasks, {}]
-        }
-    }
 
-})
+const projectSlice = createSlice({
+  name: "project",
+  initialState: <projectState>{
+    id: "",
+    tasks: [],
+  },
+  reducers: {
+    addTask(state, action) {
+      const id = action.payload.user + window.performance.now();
+      const emptyTask: task = {
+        id,
+        name: "",
+        prerequisites: [],
+        files: [],
+      };
+      state.tasks = [...state.tasks, emptyTask];
+    },
+    updateTaskName(state, action) {
+      const taskIndex = state.tasks.findIndex(
+        task => task.id == action.payload.id
+      );
+      state.tasks[taskIndex].name = action.payload.name;
+    },
+  },
+});
+
+const actions = projectSlice.actions;
+const projectSlicerReducer = projectSlice.reducer;
